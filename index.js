dae79f80458145e7a4b6ce8185264618bde1af4b
github username <github email address>
2019-08-06 09:22:56 -0400

    Done

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-// Your code here
+// Your code
+// var dodger = document.getElementById("dodger");
+var dodger = document.querySelector("#dodger");
+var game = document.querySelector("#game");
+
+function styleDodgerColor() {
+    dodger.style.backgroundColor = "#FF69B4";
+}
+
 function moveDodgerLeft() {
   var leftNumbers = dodger.style.left.replace("px", "");
   var left = parseInt(leftNumbers, 10);
@@ -8,12 +16,25 @@ function moveDodgerLeft() {
   }
 }
 
-// dodger.offsetWidth == 40
+// dodger.clientWidth == 40
 function moveDodgerRight() {
-  var rightNumbers = dodger.style.left.replace("px", "");
-  var right = parseInt(rightNumbers, 10);
+  var leftNumbers = dodger.style.left.replace("px", "");
+  var right = parseInt(leftNumbers, 10) + dodger.clientWidth;
 
-  if (right > 0) {
-    dodger.style.right = `${right + 41}px`;
+  if (right < game.clientWidth) {
+    dodger.style.left = `${parseInt(leftNumbers, 10) + 1}px`;
   }
 }
+
+function addEventListener() {
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+}
+
+styleDodgerColor();
+addEventListener();
