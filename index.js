3e0c47fa7de00a081905b27feac3b84a3a62bc2f
github username <github email address>
2019-08-05 20:47:16 -0400

    Not done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace("px", "");
+  var left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+// dodger.offsetWidth == 40
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.left.replace("px", "");
+  var right = parseInt(rightNumbers, 10);
+
+  if (right > 0) {
+    dodger.style.right = `${right + 41}px`;
+  }
+}
